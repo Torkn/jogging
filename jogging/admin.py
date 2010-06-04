156717992413eeed45cdc19ce0c9fc8bbe6c52e4
Stephen Tyler <torkn@datagrowth.com>
2010-06-04 21:44:46 +1000

    Linking between log and summary needs more work

--- jogging/admin.py
+++ jogging/admin.py
@@ -4,7 +4,7 @@ from jogging.models import Log, LogSummary
 class LogAdmin(admin.ModelAdmin):
     date_hierarchy = 'datetime'
     model = Log
-    list_display = ['datetime', 'host', 'level', 'source', 'abbrev_msg', 'summary']
+    list_display = ['datetime', 'host', 'level', 'source', 'abbrev_msg']
     search_fields = ['source', 'msg', 'host']
     list_filter = ['level', 'source', 'host']
 
